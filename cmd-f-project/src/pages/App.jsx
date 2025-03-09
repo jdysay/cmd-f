d6f20625c81624ad51850e66166b6ba3b847f173
biancadimaano <bianca.fbcd@gmail.com>
2025-03-08 18:34:32 -0800

    Added path to Business Info Form

--- cmd-f-project/src/pages/App.jsx
+++ cmd-f-project/src/pages/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TariffCalculator from './../tariff_calculator/TariffCalculator';
+import BusinessInfoForm from './BusinessInfoForm'
+
 import './../css/App.css'
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
         <Routes>
           {/* Define a route for the TariffCalculator component */}
           <Route path="/" element={<TariffCalculator />} />
+          <Route path="business-info-form" element={<BusinessInfoForm />} />
         </Routes>
       </div>
     </Router>
---
+++ cmd-f-project/src/pages/BusinessInfoForm.jsx
@@ -0,0 +1,13 @@
+import { collection, getDocs } from "firebase/firestore"; 
+import { db } from './../config/firestore.js';
+
+function BusinessInfoForm(){
+    return(
+        <>
+            <p>HI BUSINESS INFO FORM</p>
+        </>
+    )
+
+}
+
+export default BusinessInfoForm
