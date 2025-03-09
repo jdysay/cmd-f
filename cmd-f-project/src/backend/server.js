8fa8fb83a400ae343f00346abdeecc74a6367d57
jdysay <jdysay@icloud.com>
2025-03-09 11:02:51 -0700

    update

--- cmd-f-project/src/backend/server.js
+++ cmd-f-project/src/backend/server.js
@@ -54,6 +54,6 @@ app.post('/calculate-tariff', async (req, res) => {
 });
 
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${port}`);
 });
