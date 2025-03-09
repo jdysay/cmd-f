ba902115bd5463f087848038ff6f9a00237b14a5
jdysay <jdysay@icloud.com>
2025-03-09 11:17:11 -0700

    remove

--- cmd-f-project/src/pages/App.jsx
+++ cmd-f-project/src/pages/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import TariffCalculator from "../tariff_calculator/TariffCalculator";
